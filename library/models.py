22b195c31f29b3136bd5f2f6b749a2d0bea7b034
Sardordev022 <sardordev022@gmail.com>
2024-07-25 10:33:33 +0500

    initial commit

---
+++ library/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class BookModel(models.Model):
+    
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=13)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.autor} : {self.title}"
+    
+    class Meta:
+        db_table = 'Books'
+        managed = True
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
